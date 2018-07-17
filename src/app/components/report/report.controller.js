3ece3292320fc745756bc296eb1b3accdf20b2b2
Andy-home-pc <xmtxu@cn.ibm.com>
2018-07-17 11:52:03 +0800

    edit report list page flow layout and add empty page and mockup data

--- src/app/components/report/report.controller.js
+++ src/app/components/report/report.controller.js
@@ -3,9 +3,16 @@ export default class ReportController {
     // static $inject = ['http'];
     constructor($rootScope, $scope, $state) {
         [this.$rootScope, this.$scope, this.routerType, this.name] = [$rootScope, $scope, $state.current.routerType, 'ReportController'];
-        this.reportList = [[{'name':'Subsystem A'}, {'name':'Subsystem B'}, {'name':'Subsystem B'}],[{'name':'Subsystem A'}, {'name':'Subsystem B'}, {'name':'Subsystem B'}]];
+        this.reportList = [
+                            [{'name':'DSN',  'missingHipers':661, 'PEs':16, 'inconsistencies':91, 'score':0, 'release':'b.1', 'reportPeriod':'2015-12-1', 'reportStatus':'New'}, 
+                             {'name':'DSN2', 'missingHipers':0, 'PEs':6, 'inconsistencies':20, 'score':90, 'release':'b.1', 'reportPeriod':'2015-12-1', 'reportStatus':'New'}, 
+                             {'name':'DSN3', 'missingHipers':0, 'PEs':3, 'inconsistencies':6, 'score':95, 'release':'b.1', 'reportPeriod':'2015-12-1', 'reportStatus':'New'}
+                            ],
+                            [{'name':'DSN4', 'missingHipers':0, 'PEs':0, 'inconsistencies':0, 'score':100, 'release':'b.1', 'reportPeriod':'2015-12-1', 'reportStatus':'New'}, ]
+                          ];
         $scope.$parent.$parent.$ctrl.routerType = $state.current.routerType;
     }
+    
     login() {
         // this.http.get({userName: 'link', userPassword: '23333'}, url.login, (data) => {
         //     console.log(data)
