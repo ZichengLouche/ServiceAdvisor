403877766721e4e5aa5546fd9135163d8f52d5d1
Andy-home-pc <xmtxu@cn.ibm.com>
2018-07-31 15:36:36 +0800

    format report date

--- src/app/components/report/report.controller.js
+++ src/app/components/report/report.controller.js
@@ -4,11 +4,11 @@ export default class ReportController {
     constructor($rootScope, $scope, $state) {
         [this.$rootScope, this.$scope, this.routerType, this.name] = [$rootScope, $scope, $state.current.routerType, 'ReportController'];
         this.reportList = [
-                            [{'name':'DSN',  'missingHipers':20, 'PEs':13, 'inconsistencies':10, 'score':0, 'release':'a.1', 'reportPeriod':'2014-10-1', 'reportStatus':'New'}, 
-                             {'name':'DSN2', 'missingHipers':33, 'PEs':14, 'inconsistencies':0, 'score':95, 'release':'a.1', 'reportPeriod':'2016-12-1', 'reportStatus':'New'}, 
-                             {'name':'DSN3', 'missingHipers':56, 'PEs':23, 'inconsistencies':0, 'score':90, 'release':'b.1', 'reportPeriod':'2015-08-1', 'reportStatus':'New'}
+                            [{'name':'DSN',  'missingHipers':20, 'PEs':13, 'inconsistencies':10, 'score':0, 'release':'a.1', 'reportPeriod':new Date('2017-12-1'), 'reportStatus':'New'}, 
+                             {'name':'DSN2', 'missingHipers':33, 'PEs':14, 'inconsistencies':0, 'score':95, 'release':'a.1', 'reportPeriod':new Date('2016-09-12'), 'reportStatus':'New'}, 
+                             {'name':'DSN3', 'missingHipers':56, 'PEs':23, 'inconsistencies':0, 'score':90, 'release':'b.1', 'reportPeriod':new Date('2015-08-21'), 'reportStatus':'New'}
                             ],
-                            [{'name':'DSN4', 'missingHipers':54, 'PEs':20, 'inconsistencies':0, 'score':90, 'release':'c.1', 'reportPeriod':'2017-11-1', 'reportStatus':'New'}, ]
+                            [{'name':'DSN4', 'missingHipers':54, 'PEs':20, 'inconsistencies':0, 'score':90, 'release':'c.1', 'reportPeriod':new Date('2014-11-09'), 'reportStatus':'New'}, ]
                           ];
         $scope.$parent.$parent.$ctrl.routerType = $state.current.routerType;
     }
